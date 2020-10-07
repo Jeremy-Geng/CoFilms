e92ab8e1ba07dca3c0ace822f7e8961855c3882a
Shuhao Geng <u6725016@anu.edu.au>
2020-10-07 11:48:41 +0800

    Question and Answers Partched

--- app/src/main/java/com/cognidius/cofilms/database/room/CoFilmsDatabase.java
+++ app/src/main/java/com/cognidius/cofilms/database/room/CoFilmsDatabase.java
@@ -4,9 +4,13 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
 import com.cognidius.cofilms.database.room.dao.UserDao;
+import com.cognidius.cofilms.database.room.dao.VideoDao;
 
-@Database(entities = {User.class}, version = 1)
+@Database(entities = {User.class, Video.class}, version = 1)
 public abstract class CoFilmsDatabase extends androidx.room.RoomDatabase {
     public abstract UserDao userDao();
+
+    public abstract VideoDao videoDao();
+
 
 }
